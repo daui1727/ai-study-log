aaa2c160395633c2c228202de6db1c89977ffb7c
daui1727 <daui1727@gmail.com>
2026-09-02 22:18:37 +0900

    codeup test 21~98문

--- codeup_test.py
+++ codeup_test.py
@@ -1,5 +1,15 @@
-a = int(input())
+n = int(input()) # n번 무작위로 부른 횟수
+a = input().split # n번만큼 부른 출석 번호
 
-for i in range(1, a + 1):
-	if i % 3 != 0:
-		print(i, end=' ')
+for i in range(n):
+	a[i] = int(a[i])
+
+b = []
+for i in range(24):
+	b.append(0)
+
+for i in range(n):
+	b[a[i]] += 1
+
+for i in range(1, 24):
+	print(b[i], end = ' ')
